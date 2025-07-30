5c2e4b02c86b0f7ab48a3b17afbfe0d31934cbdf
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2025-07-29 21:49:44 -0300

    codigo nome em python

---
+++ python/nome.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+cor = input('Qual a sua cor favorita? ')
+print('Olá,', nome, 'de', idade, 'anos, que tem a cor favorita', cor)
