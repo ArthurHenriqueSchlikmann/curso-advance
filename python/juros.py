9386d70b4233f96080d125bc4d934f72d4715ec4
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2025-07-31 22:39:32 -0300

    ajuste fino e desnecessario, mas agora a output de ambos está igual

--- python/juros.py
+++ python/juros.py
@@ -3,7 +3,7 @@ from numpy import double
 # Pede ao usuário o capital inicial, a taxa de juros e o tempo de aplicação
 capitalInicial = int(input('Digite o capital inicial da aplicação: '))
 taxaJuros = double(input('Digite a taxa de juros: ')) / 100
-tempo = int(input('Digite o tempo de aplicação em anos: '))
+tempo = int(input('Digite o tempo de aplicação (em anos): '))
 # Calcula o montante final usando a fórmula do montante
 montante = capitalInicial * (1 + taxaJuros) ** tempo
 # Exibe o montante final
