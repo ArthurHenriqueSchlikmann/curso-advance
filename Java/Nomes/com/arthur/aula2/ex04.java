0091e910753be64e23681dc63c3f796964ca3bce
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2025-08-11 19:56:45 -0300

    exercícios

---
+++ Java/Nomes/com/arthur/aula2/ex04.java
@@ -0,0 +1,8 @@
+package com.arthur.aula2;
+
+public class ex04 {
+    public static void main(String[] args) {
+        double pi1000 = Math.pow(Math.PI, 1000);
+        System.out.println("O valor de PI elevado a 1000 é: " + pi1000);
+    }
+}
